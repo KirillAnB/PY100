7bfb5c9d68c8a52ece65f003074db5059c9c1299
KirillAnd <74457027+KirillAnB@users.noreply.github.com>
2022-10-25 15:50:25 +0300

    Add files via upload
    Не справился с задачей 8, получаю 6 месяцев. Ошибка где-то в цикле.

---
+++ Лабораторная работа/task9.py
@@ -0,0 +1,18 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+# TODO Оформить решение
+
+def how_to_survive2(bag, life, time, cash, infl):
+    for i in range(1, time+1):
+        bag += life
+        bag -= cash
+        life = life + life * infl
+    return bag
+
+answer = how_to_survive2(need_money, spend, months, salary, increase)
+print(round(answer))
